9a6900026574ac2a31db412d4274f94e824b2a94
gordonmcvey <grj.mcvey@gmail.com>
2025-08-26 12:59:28 +0100

    Minor code changes to support .env file being absent

--- examples/vanilla/app/public/index.php
+++ examples/vanilla/app/public/index.php
@@ -42,7 +42,7 @@ use gordonmcvey\WarpCore\ShutdownHandler;
 require_once __DIR__ . "/../vendor/autoload.php";
 
 $dotenv = Dotenv::createImmutable(__DIR__ . "/../");
-$dotenv->load();
+$dotenv->safeLoad();
 
 $dotenv->ifPresent("ERROR_REPORTING")->isInteger();
 error_reporting((int) $_ENV["ERROR_REPORTING"]);
